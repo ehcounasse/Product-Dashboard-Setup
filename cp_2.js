1b0eb3622ea18cbf6168ee983571bfe00637e0a8
Ethan Counasse <ehcounasse@usf.edu>
2025-10-19 23:22:18 -0400

    Using async and await

--- cp_2.js
+++ cp_2.js
@@ -15,3 +15,16 @@ function fetchProductsThen() {
         console.error("Fetch error:", error.message);
     });
 }
+
+async function fetchProductsAsync(){
+try {
+    const response = await fetch('https://www.course-api.com/javascript-store-products')
+    if (!response.ok) {
+        throw new Error ("Failed to fetch products")
+    }
+    const products = await response.json();
+    displayProducts(products);
+} catch (error) {
+    handleError(error)
+}
+}
